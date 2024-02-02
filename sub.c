28f675712c35e9abc1839c0db4d6d99ad20d6939
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-02 11:30:10 +0800

    cimmiting

--- sub.c
+++ sub.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
- * add - Add top 2 node values
+ * sub - Subtract top 2 node values
  * @stack: The stack
  * @line_number: The line of command in file
- * Description: Permits to add the first 2 nodes and store to 2nd node.
+ * Description: Permits to subtract the first 2 nodes and store to 2nd node.
  */
 void add(stack_t **stack, int line_number)
 {
@@ -11,10 +11,10 @@ stack_t *tmp;
 
 if ((*stack) == NULL || (*stack)->next == NULL)
 {
-printf("L%d: can't add, stack too short\n", line_number);
+printf("L%d: can't sub, stack too short\n", line_number);
 exit(EXIT_FAILURE);
 }
 tmp = (*stack)->next;
-tmp->n = (*stack)->n + tmp->n;
+tmp->n = tmp->n - (*stack)->n;
 pop(stack, line_number);
 }
